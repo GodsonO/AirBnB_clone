97bba5e024ca80f747d9f3ca0f89947db554b2d9
GodsonO <godsonomoghene15@gmail.com>
2023-08-12 14:41:20 -0700

    airbnb

--- models/base_model.py
+++ models/base_model.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
 """Defines the BaseModel class."""
-from models import storage
+import models
 from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel:
-    """Represents the BaseModel of the HBnB project."""
+    """The BaseModel of the HBnB project."""
 
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel.
+        """Initialization of the BaseModel.
         Args:
             *args (any): Unused.
-            **kwargs (dict): Key/value pairs of attributes.
+            **kwargs (dict): Key-value pairs of attributes.
         """
-        tform = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
         if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, tform)
+            for key, v in kwargs.items():
+                if key == "created_at":
+                    self.__dict__[key] = datetime.strptime(
+                        v, "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(
+                        v, "%Y-%m-%dT%H:%M:%S.%f")
                 else:
-                    self.__dict__[k] = v
+                    self.__dict__[key] = v
         else:
             models.storage.new(self)
 
